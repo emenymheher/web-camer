a4628f098f17bc515d9de622d324811d5a20b690
emenymheher <125128511+emenymheher@users.noreply.github.com>
2023-02-27 21:28:12 +0200

    Add files via upload

---
+++ New folder (6)/scrpt.js
@@ -0,0 +1,16 @@
+let img=document.querySelector('.img')
+let conteiner =document.querySelector('.conteiner')
+
+
+function phones(phone){
+
+img.src = phone;
+
+
+}
+function colors(color){
+
+
+    conteiner.style.background = color;
+
+}
